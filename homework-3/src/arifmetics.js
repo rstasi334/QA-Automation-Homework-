18316c44d5637a3f4ea246d04cab42c428cf003f
rstasi334 <rstasi334@gmail.com>
2025-02-03 15:37:57 +0100

    homework3 (#2)
    * homework3
    
    * homework3 (fixed lint issues)
    
    ---------
    
    Co-authored-by: Stanislav Rodzin <stanislaw.rodzin@gmail.com>

---
+++ homework-3/src/arifmetics.js
@@ -0,0 +1,11 @@
+function NumbersAndStrings() {
+    const a = Number('5');
+    const b = Number('10');
+    const c = String(20);
+    const d = String(30);
+
+    console.log(a + b, b - a, a + c, d + c, a * b, d / a);
+
+}
+
+NumbersAndStrings();
